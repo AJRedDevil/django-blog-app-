3ac4ecfcf1545584ce94e4984eb2fb507759757a
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-09-09 21:17:26 +0545

    Heroku config

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressionManifestStaticFileStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressionManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
